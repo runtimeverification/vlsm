b4b49f3c55290051e3eaf6a1ba87a84dd170361b
Dafina Trufas <dafinatrufas@gmail.com>
2022-07-25 18:54:08 +0300

    Fixing the reversion problem

--- scripts/duplicates.sh
+++ scripts/duplicates.sh
@@ -5,8 +5,8 @@ echo "| Number of occurrences | Filename | Paths |"
 echo "|:----------------------|:---------|:------|"
 for i in $DUPLICATES
   do
-    echo  -n " $i " 
-    find . -iname "$i" -exec echo -n $1 {} \; | sed 's/\t/\t | /g; s/^/| /g; s/$/ |/g' # the paths to the files with the current name
+    echo  -n " | $i " 
+    find . -iname "$i" -exec echo -n "$1" {} \; | sed 's/\t/\t | /g; s/^/| /g; s/$/ |/g' # the paths to the files with the current name
     if ! is_int "$i" # if the value of variable i at the current iteration is not an integer, then it means it is a string (filename)
     then 
       echo
