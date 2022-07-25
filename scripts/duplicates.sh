9f357e4cad46bf6fa98b6b2a03d52ae831a92ed5
Dafina Trufas <dafinatrufas@gmail.com>
2022-07-25 13:58:22 +0300

    Addressing reviews 2

--- scripts/duplicates.sh
+++ scripts/duplicates.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-BASE=$(dirname $0)
-echo "| Number of occurrences | Filename | Paths |"
-echo "|:----------------------|:---------|:------|"
 is_int() { test "$@" -eq "$@" 2> /dev/null; } 
 DUPLICATES=$(find . -iname "*.v" -type f -printf "%f\n" | sort | uniq -cd) # a list of the duplicate filenames and their frequencies
+echo "| Number of occurrences | Filename | Paths |"
+echo "|:----------------------|:---------|:------|"
 for i in $DUPLICATES
   do
-    echo  -n " $i " # the number of occurences / the filename
+    echo  -n " $i " 
     find . -iname "$i" -exec echo -n $1 {} \; | sed 's/\t/\t | /g; s/^/| /g; s/$/ |/g' # the paths to the files with the current name
-    if ! is_int "$i" # if the value of variable i at the current iteration is a string (filename), move on a new line 
+    if ! is_int "$i" # if the value of variable i at the current iteration is not an integer, then it means it is a string (filename), move on a new line 
     then 
       echo
     fi
