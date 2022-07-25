e76718634bf8362b0a7f6ff04c88b9d07ec2557c
Dafina Trufas <dafinatrufas@gmail.com>
2022-07-25 11:10:16 +0300

    Addressing reviews

--- scripts/duplicates.sh
+++ scripts/duplicates.sh
@@ -3,13 +3,13 @@ BASE=$(dirname $0)
 echo "| Number of occurrences | Filename | Paths |"
 echo "|:----------------------|:---------|:------|"
 is_int() { test "$@" -eq "$@" 2> /dev/null; } 
-DUPLICATES=$(find . -iname "*.v" -type f -printf "%f\n" | sort | uniq -cd)
+DUPLICATES=$(find . -iname "*.v" -type f -printf "%f\n" | sort | uniq -cd) # a list of the duplicate filenames and their frequencies
 for i in $DUPLICATES
-    do
-        echo  -n " $i "
-        find . -iname "$i" -exec $BASE/filenames.sh "{}" \; | sed 's/\t/\t | /g; s/^/| /g; s/$/ |/g'
-        if ! is_int "$i"
-        then 
-            echo
-        fi
-    done
+  do
+    echo  -n " $i " # the number of occurences 
+    find . -iname "$i" -exec echo -n $1 {} \; | sed 's/\t/\t | /g; s/^/| /g; s/$/ |/g' # the paths to the files with the current name
+    if ! is_int "$i" # if the value of variable i at the current iteration is a string (filename), move on a new line 
+    then 
+      echo
+    fi
+  done
--- scripts/filenames.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-NAME=$1
-echo -n " $NAME " 
