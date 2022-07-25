fa21f56a56c8676d1138824186d03ae5ccdcc206
wkolowski <wkolowski@protonmail.com>
2022-07-25 19:38:30 +0200

    Finishing touches to ensure everything prints and renders correctly. Added a lot of comments.

--- scripts/duplicates.sh
+++ scripts/duplicates.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
-is_int() { test "$@" -eq "$@" 2> /dev/null; } 
-DUPLICATES=$(find . -iname "*.v" -type f -printf "%f\n" | sort | uniq -cd) # a list of the duplicate filenames and their frequencies
+
+# The list of duplicate filenames and their occurrence counts,
+# sorted from most to least frequent.
+duplicates=$(find . -iname "*.v" -type f -printf "%f\n" | sort -nr | uniq -cd)
+
+# We will output the result as a markdown table. First comes the header.
 echo "| Number of occurrences | Filename | Paths |"
-echo "|:----------------------|:---------|:------|"
-for i in $DUPLICATES
-  do
-    echo  -n " | $i " 
-    find . -iname "$i" -exec echo -n "$1" {} \; | sed 's/\t/\t | /g; s/^/| /g; s/$/ |/g' # the paths to the files with the current name
-    if ! is_int "$i" # if the value of variable i at the current iteration is not an integer, then it means it is a string (filename)
-    then 
-      echo
-    fi
-  done
-  
+echo "| --------------------- | -------- | ----- |"
+
+# Iterate over the list.
+# BEWARE: the list contains both numbers (occurence counts) and strings (filenames).
+for i in $duplicates
+do
+  # Fill the first two columns of the current row.
+  echo  -n "| $i "
+
+  # Find paths of all files with the same name as $i
+  # and print them (separated by spaces).
+  find . -iname "$i" -exec echo -n "$1" {} \; \
+  `# Now some hacky post-processing.` \
+  | sed \
+  `# Add a pipe at the beginning to mark the start of the last column.` \
+  -e 's/^/|/' \
+  `# Separate the paths with a <br> tag rather than spaces so that they are rendered on separate lines.` \
+  -e 's/v \./v <br> ./g' \
+  `# Add a space before that last pipe and add a newline character.` \
+  -e 's/$/ |\n/g'
+done
