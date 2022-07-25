8a425f3727d49eab2c26b5412b1ba283ee4eaa05
Dafina Trufas <dafinatrufas@gmail.com>
2022-07-25 11:45:06 +0300

    Addressing reviews 1

--- scripts/duplicates.sh
+++ scripts/duplicates.sh
@@ -6,7 +6,7 @@ is_int() { test "$@" -eq "$@" 2> /dev/null; }
 DUPLICATES=$(find . -iname "*.v" -type f -printf "%f\n" | sort | uniq -cd) # a list of the duplicate filenames and their frequencies
 for i in $DUPLICATES
   do
-    echo  -n " $i " # the number of occurences 
+    echo  -n " $i " # the number of occurences / the filename
     find . -iname "$i" -exec echo -n $1 {} \; | sed 's/\t/\t | /g; s/^/| /g; s/$/ |/g' # the paths to the files with the current name
     if ! is_int "$i" # if the value of variable i at the current iteration is a string (filename), move on a new line 
     then 
