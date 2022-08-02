d12e69750184eefad2d9e04bf080b107d6454d17
Dafina Trufas <dafinatrufas@gmail.com>
2022-08-02 18:01:46 +0300

    Removing lemmas from RealsExtras.v

--- theories/VLSM/Lib/RealsExtras.v
+++ theories/VLSM/Lib/RealsExtras.v
@@ -4,20 +4,20 @@ From stdpp Require Import prelude.
 (** * Real number utility lemmas *)
 
 (** This lemma is needed in fault_weight_state_backwards **)
-Lemma Rplusminus_assoc : forall r1 r2 r3,
+(*Lemma Rplusminus_assoc : forall r1 r2 r3,
   (r1 + r2 - r3)%R = (r1 + (r2 - r3))%R.
 Proof.
   intros. unfold Rminus.
   apply Rplus_assoc.
-Qed.
+Qed.*)
 
 (** This lemma is needed in fault_weight_state_sorted_subset **)
-Lemma Rplusminus_assoc_r : forall r1 r2 r3,
+(*Lemma Rplusminus_assoc_r : forall r1 r2 r3,
   (r1 - r2 + r3)%R = (r1 + (- r2 + r3))%R.
 Proof.
   intros. unfold Rminus.
   apply Rplus_assoc.
-Qed.
+Qed.*)
 
 (** This lemma is needed in fault_weight_state_sorted_subset **)
 Lemma Rplus_opp_l : forall r, (Ropp r + r)%R = 0%R.
@@ -28,7 +28,7 @@ Proof.
 Qed.
 
 (** This lemma is needed in fault_weight_state_sorted_subset **)
-Lemma Rplus_ge_reg_neg_r : forall r1 r2 r3,
+(*Lemma Rplus_ge_reg_neg_r : forall r1 r2 r3,
   (r2 <= 0)%R -> (r3 <= r1 + r2)%R -> (r3 <= r1)%R.
 Proof.
   intros.
@@ -36,26 +36,26 @@ Proof.
   apply Rle_ge in H.
   apply Rle_ge in H0.
   apply (Rplus_ge_reg_neg_r r1 r2 r3 H H0).
-Qed.
+Qed.*)
 
 (** This lemma is needed in fault_weight_state_sorted_subset **)
-Lemma Rminus_lt_r : forall r1 r2,
+(*Lemma Rminus_lt_r : forall r1 r2,
   (0 <= r2)%R -> (r1 - r2 <= r1)%R.
 Proof.
   intros.
   rewrite <- Rplus_0_r.
   unfold Rminus.
   by apply Rplus_le_compat_l, Rge_le, Ropp_0_le_ge_contravar.
-Qed.
+Qed.*)
 
-Lemma Rminus_lt_r_strict : forall r1 r2,
+(*Lemma Rminus_lt_r_strict : forall r1 r2,
   (0 < r2)%R -> (r1 - r2 <= r1)%R.
 Proof.
   intros.
   rewrite <- Rplus_0_r.
   unfold Rminus.
   by apply Rplus_le_compat_l, Rge_le, Ropp_0_le_ge_contravar, Rlt_le.
-Qed.
+Qed.*)
 
 Lemma Rtotal_le_gt : forall x y,
   (x <= y)%R \/ (x > y)%R.
