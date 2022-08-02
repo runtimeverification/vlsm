1e88e8daf40cba82a5355399da7f9f8bdc3c4967
Dafina Trufas <dafinatrufas@gmail.com>
2022-08-02 18:15:37 +0300

    Removing lemmas from SsrExport.v

--- theories/VLSM/Lib/SsrExport.v
+++ theories/VLSM/Lib/SsrExport.v
@@ -4,4 +4,4 @@ From Coq Require Export ssreflect.
 #[export] Set SsrOldRewriteGoalsOrder.
 #[export] Set Asymmetric Patterns.
 #[export] Set Bullet Behavior "None".
-Definition iYC2 := True.
+(*Definition iYC2 := True.*)
