4b61ee71f68dc6a71660212da2e5d1a13beae875
TeodorescuIoan <ioanoctavian01@gmail.com>
2023-03-16 16:21:15 +0200

    Add separator options to comment script

--- scripts/all-comment-ratio.sh
+++ scripts/all-comment-ratio.sh
@@ -1,7 +1,35 @@
 #!/bin/sh
 #| Paper           | Coq                              |
 #|:----------------|:---------------------------------|
+
+# Usage: all-comment-ratio.sh [--separator tab|pipe] | [-s tab|pipe]
+
+
+VALID_ARGS=$(getopt -o s: --long separator: -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+SEP="\t"
+
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+  case "$1" in
+    -s | --separator)
+        echo "Processing 'gamma' option. Input argument is '$2'"
+        echo $2
+        if [[ "$2" == "tab" ]]; then SEP="\t"; else SEP="|"; fi
+        shift 2
+        ;;
+    --) shift; 
+        break 
+        ;;
+  esac
+done
+
 BASE=$(dirname $0)
 echo "| Comments size | Spec size | Comments-Spec Ratio | Spec-Comments Ratio | Filename |"
 echo "|:--------------|:----------|:--------------------|:--------------------|:---------|"
-find . -iname "*.v" -exec $BASE/comment-ratio.sh "{}" \; | sort -n -k3 | sed 's/\t/\t | /g; s/^/| /g; s/$/ |/g'
+find . -iname "*.v" -exec $BASE/comment-ratio.sh $SEP "{}" \; | sort -n -k3 | sed 's/\t/\t | /g; s/^/| /g; s/$/ |/g'
+
+
--- scripts/comment-ratio.sh
+++ scripts/comment-ratio.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-NAME=$1 
+
+SEP=$1
+
+NAME=$2
+
 COMMENTS=$(coqwc $NAME|awk 'FNR == 2{print $3}')
 SPEC=$(coqwc $NAME|awk 'FNR == 2{print $1}')
 RATIOCS=$(echo "scale=2; $COMMENTS/($SPEC+0.1)" | bc)
 RATIOSC=$(echo "scale=2; $SPEC/($COMMENTS+0.1)" | bc)
-echo "$COMMENTS\t$SPEC\t$RATIOCS\t$RATIOSC\t$NAME"
+echo "$COMMENTS$SEP$SPEC$SEP$RATIOCS$SEP$RATIOSC$SEP$NAME"
