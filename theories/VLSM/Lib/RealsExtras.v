2b062cbae38f20f2e2456d3177f0a4c053411937
wkolowski <wkolowski@protonmail.com>
2023-04-24 19:28:56 +0200

    Fix.

--- theories/VLSM/Lib/RealsExtras.v
+++
@@ -1,9 +0,0 @@
-From Coq Require Import Reals.
-From stdpp Require Import prelude.
-
-(** * Real number utility definitions and lemmas *)
-
-#[export] Instance Rle_transitive : Transitive Rle.
-Proof.
-  by intros x y z; apply Rle_trans.
-Qed.
