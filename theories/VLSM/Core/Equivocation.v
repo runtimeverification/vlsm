56aeedbfb457f983dbac6e3855ecefcf559309c8
wkolowski <wkolowski@protonmail.com>
2023-06-28 17:20:31 +0200

    Refactor the proof of composite_no_initial_valid_messages_emitted_by_sender.

--- theories/VLSM/Core/Equivocation.v
+++ theories/VLSM/Core/Equivocation.v
@@ -2026,30 +2026,6 @@ Definition sender_nontriviality_prop : Prop :=
 Definition no_initial_messages_in_IM_prop : Prop :=
   forall i m, ~ initial_message_prop (IM i) m.
 
-Lemma composite_no_initial_valid_messages_emitted_by_sender
-    (can_emit_signed : channel_authentication_prop)
-    (no_initial_messages_in_IM : no_initial_messages_in_IM_prop)
-    (constraint : composite_label IM -> composite_state IM * option message -> Prop)
-    (X := composite_vlsm IM constraint)
-    : forall (m : message), valid_message_prop X m ->
-      exists v, sender m = Some v /\
-        can_emit (pre_loaded_with_all_messages_vlsm (IM (A v))) m.
-Proof.
-  intro m.
-  rewrite emitted_messages_are_valid_iff.
-  intros [[i [[mi Hmi] _]] | [(s, om) [(i, l) [s' Ht]]]]
-  ; [by contradict Hmi; apply no_initial_messages_in_IM |].
-  apply (VLSM_incl_input_valid_transition (constraint_preloaded_free_incl IM _)) in Ht.
-  apply pre_loaded_with_all_messages_projection_input_valid_transition_eq
-    with (j := i) in Ht; [| done]; cbn in Ht.
-  specialize (can_emit_signed i m).
-  spec can_emit_signed; [by eexists _, _, _ |].
-  unfold channel_authenticated_message in can_emit_signed.
-  destruct (sender m) as [v |] eqn: Hsender; [| by inversion can_emit_signed].
-  apply Some_inj in can_emit_signed.
-  by exists v; subst; unfold can_emit; eauto.
-Qed.
-
 Lemma free_composite_no_initial_valid_messages_emitted_by_sender
   (can_emit_signed : channel_authentication_prop)
   (no_initial_messages_in_IM : no_initial_messages_in_IM_prop)
@@ -2072,6 +2048,21 @@ Proof.
   destruct (sender m) as [v |] eqn: Hsender; [| by inversion can_emit_signed].
   apply Some_inj in can_emit_signed.
   by exists v; subst; unfold can_emit; eauto.
+Qed.
+
+Lemma composite_no_initial_valid_messages_emitted_by_sender
+    (can_emit_signed : channel_authentication_prop)
+    (no_initial_messages_in_IM : no_initial_messages_in_IM_prop)
+    (constraint : composite_label IM -> composite_state IM * option message -> Prop)
+    (X := composite_vlsm IM constraint)
+    : forall (m : message), valid_message_prop X m ->
+      exists v, sender m = Some v /\
+        can_emit (pre_loaded_with_all_messages_vlsm (IM (A v))) m.
+Proof.
+  intros.
+  apply free_composite_no_initial_valid_messages_emitted_by_sender; [done.. |].
+  eapply VLSM_incl_valid_message with X; [| by do 2 red | done].
+  by apply constraint_free_incl.
 Qed.
 
 Lemma composite_no_initial_valid_messages_have_sender
