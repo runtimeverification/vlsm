c91d55b24c486374f0ee6a158bda2e5d73100a42
wkolowski <wkolowski@protonmail.com>
2023-07-14 14:27:13 +0200

    Fix FixedSetByzantineTraces.v

--- theories/VLSM/Core/ByzantineTraces/FixedSetByzantineTraces.v
+++ theories/VLSM/Core/ByzantineTraces/FixedSetByzantineTraces.v
@@ -562,8 +562,7 @@ Proof.
   cbn; destruct (sender m) as [v |] eqn: Hsender; [| done]; cbn.
   case_decide as HAv; [| done].
   unfold sub_IM; cbn.
-  apply (VLSM_incl_valid_state (VLSM_incl_constrained_vlsm IM
-    (fixed_equivocation_constraint IM selection))) in Hs.
+  eapply VLSM_incl_valid_state in Hs; [| by apply VLSM_incl_constrained_vlsm].
   apply (VLSM_incl_valid_state (vlsm_incl_pre_loaded_with_all_messages_vlsm
     (free_composite_vlsm IM))) in Hs.
   assert (Hpre_si : forall i, valid_state_prop (pre_loaded_with_all_messages_vlsm (IM i)) (s i)).
