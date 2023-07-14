0d5bd8f3cbe9621391df42f0048471fd2efd8fc1
wkolowski <wkolowski@protonmail.com>
2023-07-14 14:30:12 +0200

    Fix LimitedMessageEquivocation.v

--- theories/VLSM/Core/Equivocation/LimitedMessageEquivocation.v
+++ theories/VLSM/Core/Equivocation/LimitedMessageEquivocation.v
@@ -219,7 +219,7 @@ Proof.
   assert (StrongFixedinclPreFree : VLSM_incl StrongFixed PreFree).
   {
     apply VLSM_incl_trans with Free.
-    - by apply (VLSM_incl_constrained_vlsm IM (strong_fixed_equivocation_constraint IM equivocators)).
+    - by apply VLSM_incl_constrained_vlsm.
     - by apply vlsm_incl_pre_loaded_with_all_messages_vlsm.
   }
   apply valid_state_has_trace in Hs as [is [tr Htr]].
@@ -368,7 +368,7 @@ Proof.
   eapply traces_exhibiting_limited_equivocation_are_valid_rev; [done.. | |].
   - apply valid_trace_add_default_last.
     eapply VLSM_incl_finite_valid_trace; [| done].
-    by apply VLSM_incl_constrained_vlsm.
+    by apply (VLSM_incl_constrained_vlsm (free_composite_vlsm IM)).
   - by apply tracewise_not_heavy_LimitedEquivocationProp_iff,
       full_node_limited_equivocation_valid_state_weight,
       finite_valid_trace_last_pstate with (X := Limited), Htr.
@@ -390,8 +390,11 @@ Lemma limited_valid_state_has_trace_exhibiting_limited_equivocation
     exists is tr, finite_trace_last is tr = s /\ fixed_limited_equivocation_prop is tr.
 Proof.
   intros s Hs.
-  assert (Hfree_s : valid_state_prop (free_composite_vlsm IM) s)
-    by (revert Hs; apply VLSM_incl_valid_state, VLSM_incl_constrained_vlsm).
+  assert (Hfree_s : valid_state_prop (free_composite_vlsm IM) s).
+  {
+    apply VLSM_incl_valid_state; [| done].
+    by apply (VLSM_incl_constrained_vlsm (free_composite_vlsm IM)).
+  }
   destruct
     (free_has_strong_trace_witnessing_equivocation_prop IM threshold A sender _ s Hfree_s)
     as (is & tr & Htr & Heqv).
