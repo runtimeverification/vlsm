dbd2700eff1062a82b9020cd36e7f0d457c578ce
Karl Palmskog <palmskog@gmail.com>
2023-11-17 01:17:14 +0100

    purge unnecessary code from Lib, mostly due to upstream

--- theories/Lib/EquationsExtras.v
+++ theories/Lib/EquationsExtras.v
@@ -1,22 +1,8 @@
 From Equations Require Export Equations.
+Export Equations.Prop.Logic.
 
 (** * Equations utility definitions
 
-  The definition of [inspect] is available in Equations as of version 1.3+8.16.
-  Notation <<eq:>> is not yet available (exists only in examples).
-
-  [inspect x] allows to pattern-match [x] while retaining a propositional
-  equality with [x].
-
-  See
-  #<a href="https://github.com/mattam82/Coq-Equations/blob/v1.3-8.16/theories/Prop/Logic.v#L31">
-  here
-  </a>#.
-*)
-Definition inspect {A} (a : A) : {b | a = b} :=
-  exist _ a eq_refl.
-
-(**
   The [inspect] definition is used to pack a value with a proof
   of an equality to itself. When pattern matching on the first component in
   this existential type, we keep information about the origin of the pattern
@@ -27,4 +13,4 @@ Definition inspect {A} (a : A) : {b | a = b} :=
   here
   </a>#.
 *)
-Global Notation "x 'eq:' p" := (exist _ x p) (only parsing, at level 20).
+Notation "x 'eq:' p" := (exist _ x p) (only parsing, at level 20).
